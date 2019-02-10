b1598d5afbe154dae2f5252865294548703ecb1a
Noah Venethongkham <31664445+LovableFatty@users.noreply.github.com>
2019-02-09 23:51:32 -0800

    Add files via upload
    Student File that holds student methods

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student{
+
+    public static void main(String[] args){
+        Graph roadmap = new Graph(7);
+        roadmap.addPrerequisites();
+        roadmap.addCourses();
+    }
+    int max = 0;
+    //Constructor 
+    Student(Graph roadmap) 
+    {  
+        // list of completed courses
+    } 
+}
